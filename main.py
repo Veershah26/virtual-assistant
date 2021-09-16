6076c718527027aef9012a77bf298f2c124f1ff5
Veershah26 <shah.veer18@gmail.com>
2021-09-16 23:41:30 +0530

    added functionality for greeting and wishing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 
 ttsengine = pyttsx3.init()
 
@@ -17,7 +18,43 @@ def getvoices(voice):
 
     speak("Hello Darkness My Old Friend")
 
-while True:
-    voice = int(input("Enter 1 or 2: "))
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("The Current Time Is")
+    speak(Time)
+    print(Time)
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak("The Current Date Is:")
+    speak(date)
+    speak(month)
+    speak(year)
+
+# while True:
+#     voice = int(input("Enter 1 or 2: "))
 #     speak(audio)
-    getvoices(voice)
+#     getvoices(voice)
+# time()
+
+def greeting():
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour <12:
+        speak("Good Morning Sir")
+    elif hour >=12 and hour <18:
+        speak("Good Afternoon Sir")
+    elif hour >=18 and hour < 24:
+        speak("Good Evening Sir")
+    else:
+        speak("Good Night Sir")
+
+def wishme():
+    speak("Welcome Back Sir!")
+    greeting()
+    time()
+    date()
+    speak("How May I Help You")
+
+wishme()
