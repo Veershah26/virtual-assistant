7cb366f6dae5f4ee0b9c890fc35bdc13787c2d92
Aditya Mahimkar <mahimkaradi@gmail.com>
2021-10-05 16:24:25 +0530

    Add files via upload

---
+++ modules/WallpaperScrapper.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+import getpass as gp
+import tkinter
+from tkinter.filedialog import askdirectory
+
+def WallpaperScrapper():
+    username = gp.getuser()
+    path = os.path.join(r'C:\Users', username) + r'\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'
+    tkinter.Tk().withdraw()
+    target_path = askdirectory(title='Select Folder')
+
+    if target_path:
+        for file in os.listdir(path):
+            original = os.path.join(path, file)
+            target = os.path.join(target_path, file + '.jpg')
+            shutil.copyfile(original, target)
+        print('Wallpapers copied to desired location successfully!!')
+    else:
+        print('Invalid username or folder location!!')
+
+WallpaperScrapper()
