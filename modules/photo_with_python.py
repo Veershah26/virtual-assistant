58823cb24b4f3e7631d16e43c0de83e1ca0fdda0
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-10-05 19:01:28 +0530

    Update photo_with_python.py

--- modules/photo_with_python.py
+++ modules/photo_with_python.py
@@ -2,9 +2,16 @@ import datetime
 import cv2
 
 
-t = datetime.datetime.now()
-camera = cv2.VideoCapture(0)
-for i in range(20):
-    return_value, image = camera.read()
-cv2.imwrite(f"{t.second,t.minute,t.hour,t.day,t.month}.png",image)
-del(camera)
+
+
+def photo_with_python():
+    t = datetime.datetime.now()
+    camera = cv2.VideoCapture(0)
+    for i in range(20):
+        return_value, image = camera.read()
+    cv2.imwrite(f"{t.second,t.minute,t.hour,t.day,t.month}.png",image)
+    del(camera)
+
+
+if __name__ == "__main__":
+    photo_with_python()
