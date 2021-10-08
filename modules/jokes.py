7b36ba94d84dfb7e0d151b8a23e018edb3d47eb9
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-10-08 17:39:51 +0400

    jokes.py file added

---
+++ modules/jokes.py
@@ -0,0 +1,13 @@
+from joke.jokes import *
+from random import choice
+from main import *
+
+
+def jokes():
+    random_joke = choice([geek, icanhazdad, chucknorris, icndb])()
+    # assigning random_joke variable to get random jokes
+    speak(random_joke)
+    # virtual assistant narrating it
+
+
+jokes()
