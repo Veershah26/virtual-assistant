135beba85dce0606d40f34a6a986c7acc78949d1
Jocelyn Jeriah <jjeriah@sorcero.com>
2021-10-10 12:06:31 -0400

    refactored code into methods that return strings, they can all be spoken by the assistant

--- modules/weather.py
+++ modules/weather.py
@@ -1,5 +1,4 @@
 from pyowm.owm import OWM
-from pyowm.utils import timestamps
 from dotenv import load_dotenv
 import os
 '''
@@ -11,27 +10,61 @@ load_dotenv()
 # Gets your 'API_KEY' from .env file and stores it as 'key'
 key = os.getenv("API_KEY")
 # Enter your location here in format 'City, Country'
-location = 'Richmond, US'
+location =  'London, GB' # 'Banja Luka, BA' 'Ljubljana, SI'
 # Choose between Celsius or Fahrenheit
 temp_type = 'fahrenheit'
+# Choose 'meters_sec', 'miles_hour', 'knots', 'beaufort'
+unit='miles_hour'
 
 owm = OWM(key)
+
 mgr = owm.weather_manager()
 
 # Search for current weather in any location
 observation = mgr.weather_at_place(location)
-weather= observation.weather
-
-temp = weather.temperature(temp_type) # {'temp': 75.36, 'temp_max': 78.24, 'temp_min': 72.41, 'feels_like': 76.41, 'temp_kf': None}
-detail = weather.detailed_status # broken clouds
-wind = weather.wind() # {'speed': 2.68, 'deg': 246, 'gust': 5.36}
-humidity = weather.humidity # 81
-rain = weather.rain # {}
-heat_index = weather.heat_index # None
-clouds = weather.clouds # 75
+weather = observation.weather
 
 
-# Will it be clear tomorrow at this time in my city/country?
-forecast = mgr. forecast_at_place(location, 'daily')
-# Returns boolean response
-answer = forecast.will_be_clear_at(timestamps.tomorrow()) # False
+def temp ():
+  temp = weather.temperature(temp_type)
+  return f"The current temperature is {round(temp['temp'])} degrees. Today's high is {round(temp['temp_max'])} degrees and the low is {round(temp['temp_min'])} degrees. "
+  # {'temp': 75.36, 'temp_max': 78.24, 'temp_min': 72.41, 'feels_like': 76.41, 'temp_kf': None}
+
+def detail ():
+  return f"The sky has {weather.detailed_status}." # broken clouds
+
+def rain ():
+  rain = weather.rain
+  if rain:
+    if '1h' in rain:
+      return f"{rain['1h']} millimeters of rain have fallen within the last hour."
+    if '3h' in rain:
+      return f"{rain['3h']} millimeters of rain have fallen within the last three hours."
+  else:
+    return "There is no rain in today's forecast."
+
+def pressure ():
+  pressure = weather.pressure
+  return f"The pressure today is at {pressure['press']} hectopascals."
+
+def wind ():
+  wind = weather.wind(unit)
+  if unit == "miles_hour":
+    wind_unit = "miles per hour"
+  if unit == "meters_sec":
+    wind_unit = "meters per second"
+  if unit == "knots":
+    wind_unit == "knots"
+  if unit == "beaufort":
+    wind_unit == "beauforts"
+  if 'speed' in wind:
+    return f"Today's wind speed is {round(wind['speed'])} {wind_unit}."
+  if 'gust' in wind:
+    return f"Today's gust speed is {round(wind['gust'])} {wind_unit}."
+  # {'speed': 2.68, 'deg': 246, 'gust': 5.36}
+
+def humidity ():
+  return  f"The humidity is {weather.humidity} percent." # 81
+
+def clouds ():
+  return f"Cloud coverage is at {weather.clouds} percent." # 75
